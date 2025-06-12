fc0020ce1b0fdaaea22be7ce81013f6a8761bd5a
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:05:52 +0700

    Create LocationsPage.jsx

---
+++ src/components/public/LocationsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LocationsPage = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {LocationsPage}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default LocationsPage;
