7f4b2be542d63c0e7fb369cd1eb2a349fed30ece
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:14:45 +0700

    Create ExportData.jsx

---
+++ src/components/dashboard/ExportData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExportData = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {ExportData}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default ExportData;
