6b5d1b5553cb71d908be0302b348ccb7dfed02c0
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:06:27 +0700

    Create ContactPage.jsx

---
+++ src/components/public/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {ContactPage}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default ContactPage;
