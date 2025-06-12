6bba4c624ccc947dec274e4fb1c21c8b004e5f1b
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 19:53:46 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import dari 'react-dom/client'
-import App from './App'; // Sesuaikan jalur impor jika App.jsx berada di direktori yang berbeda
-import './index.css'; // Impor file CSS
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Pastikan ekstensi .jsx
+import './index.css';
 
-// Temukan elemen root di HTML Anda (biasanya <div id="root">)
 const rootElement = document.getElementById('root');
 
-// Buat root React dan render aplikasi Anda ke dalamnya
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
