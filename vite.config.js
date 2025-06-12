d8004a4e9182fbabd123ceb1dd8ca5996f796c16
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 20:11:18 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Konfigurasi server development jika diperlukan
   server: {
-    // Port default untuk aplikasi Anda (misalnya 3000)
     port: 3000, 
-    // Mengizinkan akses dari jaringan eksternal (penting untuk mobile testing di jaringan lokal)
     host: true 
   }
 });
