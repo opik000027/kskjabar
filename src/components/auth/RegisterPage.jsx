7ff0466f1481264cf883b3e58417fd08fc284fb6
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:08:07 +0700

    Create RegisterPage.jsx

---
+++ src/components/auth/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {RegisterPage}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default RegisterPage;
