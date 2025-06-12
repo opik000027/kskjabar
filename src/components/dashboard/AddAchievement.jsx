ff58ccd066fea8e5aa3a22dcafdcfd4b408afc4b
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:36:37 +0700

    Create AddAchievement.jsx

---
+++ src/components/dashboard/AddAchievement.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddAchievement = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {AddAchievement}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default AddAchievement;
