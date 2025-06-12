e3ecf6452ecd55121766d8a6183936d6b8309d44
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:10:12 +0700

    Create MemberDetail.jsx

---
+++ src/components/dashboard/MemberDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MemberDetail = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {MemberDetail}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default MemberDetail;
