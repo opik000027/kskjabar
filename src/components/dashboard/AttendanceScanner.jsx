37f452165fa47738775ff765babe6eaa579c1542
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:10:44 +0700

    Create AttendanceScanner.jsx

---
+++ src/components/dashboard/AttendanceScanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AttendanceScanner = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {AttendanceScanner}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default AttendanceScanner;
