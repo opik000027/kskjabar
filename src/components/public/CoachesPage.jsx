e715f4b5cc156f7c95f3283f770d2641b5b72f20
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:04:58 +0700

    Create CoachesPage.jsx

---
+++ src/components/public/CoachesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CoachesPage = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {CoachesPage}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default CoachesPage;
