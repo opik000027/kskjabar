9f496aaddb956280ca2a5b5e8eab44ebf941b3d6
TAUFIK AKBAR IRAWAN <opik000027@gmail.com>
2025-06-12 15:09:01 +0700

    Create CoachDashboard.jsx

---
+++ src/components/dashboard/CoachDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CoachDashboard = () => {
+  return (
+    <div className="p-8 text-center text-gray-700">
+      <h2>Halaman {CoachDashboard}</h2>
+      <p>Konten akan ditambahkan di sini.</p>
+    </div>
+  );
+};
+
+export default CoachDashboard;
